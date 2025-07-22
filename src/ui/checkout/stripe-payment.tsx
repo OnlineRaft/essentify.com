9f1bc487ed556fdd78fcfe7f23a09e859ec478e7
OnlineRaft <apps@onlineraft.com>
2025-07-22 10:47:55 +0200

    ready

--- src/ui/checkout/stripe-payment.tsx
+++ src/ui/checkout/stripe-payment.tsx
@@ -63,19 +63,19 @@ const PaymentForm = ({
           >
             <label>
               Име и Фамилия
-            </label>
+            </label><br />
 
             <label>
-              Телефон
-            </label>
+              Телефон:
+            </label><br />
 
             <label>
-              Адрес на офис на Спиди или Еконт
-            </label>            
+              Адрес на офис на Спиди или Еконт:
+            </label><br />            
 
-            <label>
-              Email
-            </label>
+            <label htmlFor="email" className="sr-only">
+              Email:
+            </label><br />
 
             <input
               id="email"
@@ -85,6 +85,7 @@ const PaymentForm = ({
               defaultValue="delivered@resend.dev"
               placeholder="jane@example.com"
               autoComplete="email"
+              className="w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
             />
 
             <SubmitButton />
