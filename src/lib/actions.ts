104279d4a5e2677f08b3a769c48ebe21960cfc8f
OnlineRaft <apps@onlineraft.com>
2025-07-24 11:49:46 +0200

    Update actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { Resend } from "resend";
+//import { Resend } from "resend";
 // import { VercelInviteUserEmail } from "../ui/checkout/vercel-invite-user";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+//const resend = new Resend(process.env.RESEND_API_KEY);
 
 //type State = { error: string } | { data: string };
 
