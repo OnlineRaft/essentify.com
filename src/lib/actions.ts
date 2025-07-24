6fcc41328d43a162226fc2c5686461a0d0864699
OnlineRaft <apps@onlineraft.com>
2025-07-24 11:47:51 +0200

    Update actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -7,12 +7,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 //type State = { error: string } | { data: string };
 
-return  { "error" };
-
 export async function sendiii(prevState, queryData) {
   const email = queryData.get("email") as string;
 
-  const { data, error } = await resend.emails.send({
+  if (email === "1") {
+    return "data";
+  } else {
+    return "error";
+  }
+
+/*  const { data, error } = await resend.emails.send({
     from: "Vercel <vercel@resend.dev>",
     to: [email],
     subject: "Join team on Vercel",
@@ -26,4 +30,5 @@ export async function sendiii(prevState, queryData) {
   console.log(data);
 
   return { data: "Email sent!" };
+*/  
 }
