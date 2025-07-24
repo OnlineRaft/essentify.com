bb707e98501f66f23659591767ad8d536437cfd4
OnlineRaft <apps@onlineraft.com>
2025-07-24 10:02:37 +0200

    submit

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 type State = { error: string } | { data: string };
 
-export async function send(prevState, queryData) {
+export async function send(prevState: State, formData: FormData) {
   const email = formData.get("email") as string;
 
   const { data, error } = await resend.emails.send({
