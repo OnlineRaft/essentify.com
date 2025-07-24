3911fdda30c656c28655346854dc64c9c84ebf6f
OnlineRaft <apps@onlineraft.com>
2025-07-24 08:32:27 +0200

    Update stripe-payment.tsx

--- src/ui/checkout/stripe-payment.tsx
+++ src/ui/checkout/stripe-payment.tsx
@@ -2,9 +2,8 @@
 
 import clsx from "clsx";
 import { useFormStatus } from "react-dom";
-// import { send } from "../../lib/actions";
-import * as React from "react";
-// import { toast } from "sonner";
+import { send } from "../../lib/actions";
+import * as React from "react";import { toast } from "sonner";
 import type * as Commerce from "commerce-kit";
 
 export const StripePayment = ({
@@ -42,18 +41,18 @@ const PaymentForm = ({
 
 //export default function Page() {
 
-//  const [state, dispatch] = React.useActionState(send, undefined);
-//  React.useEffect(() => {
-//    if (!state) {
-//      return;
-//    }
+  const [state, dispatch] = React.useActionState(send, undefined);
+  React.useEffect(() => {
+    if (!state) {
+      return;
+    }
 
-//    if ("data" in state) {
-//     toast(state.data);
-//    } else if ("error" in state) {
-//      toast(`Error when sending email: ${state.error}`);
-//    }
-//  }, [state]);
+    if ("data" in state) {
+     toast(state.data);
+   } else if ("error" in state) {
+      toast(`Error when sending email: ${state.error}`);
+    }
+  }, [state]);
 
   return (
     <div> 
