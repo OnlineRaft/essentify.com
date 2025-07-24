f403a68f20ca2a198e5469383bba36aad8c03b9b
OnlineRaft <apps@onlineraft.com>
2025-07-24 12:08:10 +0200

    Update actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
-//import { Resend } from "resend";
-// import { VercelInviteUserEmail } from "../ui/checkout/vercel-invite-user";
+import { Resend } from "resend";
+import { VercelInviteUserEmail } from "../ui/checkout/vercel-invite-user";
 
-//const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 //type State = { error: string } | { data: string };
 
@@ -21,7 +21,7 @@ export async function sendiii(prevState, queryData) {
 const { data, error } = await resend.emails.send({
     from: "Vercel <vercel@resend.dev>",
     to: [email],
-    subject: "Join team on Vercel",
+    subject: "Join team on Vercel AAA",
     react: VercelInviteUserEmail({}),
   });
 
